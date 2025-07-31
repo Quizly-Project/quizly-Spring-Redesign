523f9b784963944c7b53e17eca4a7e69051251a5
yoo20370 <yoo20370@gmail.com>
2025-08-01 02:17:01 +0900

    JWT 토큰에서 유저 정보 가져와서 넘기도록 수정

--- src/main/java/Team9789/quizly_Spring/controller/api/quizgroup/QuizGroupApiController.java
+++ src/main/java/Team9789/quizly_Spring/controller/api/quizgroup/QuizGroupApiController.java
@@ -4,11 +4,9 @@ import Team9789.quizly_Spring.dto.quiz.QuizGroupDto;
 import Team9789.quizly_Spring.dto.ResultDto;
 import Team9789.quizly_Spring.dto.request.quizgroup.CreateQuizGroupRequest;
 import Team9789.quizly_Spring.dto.request.quizgroup.UpdateQuizGroupRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,9 +27,9 @@ public interface QuizGroupApiController {
 
     ResponseEntity<ResultDto<QuizGroupDto>> findQuizGroupOneV1(@PathVariable("quizgroupId") Long quizgroupId);
 
-    ResponseEntity<ResultDto<Long>> addQuizGroupV1(@RequestParam("Quizgroup") CreateQuizGroupRequest request);
+    ResponseEntity<ResultDto<Long>> addQuizGroupV1(@RequestParam("Quizgroup") CreateQuizGroupRequest request, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
 
-    ResponseEntity<ResultDto<Long>> updateQuizGroupV1(@RequestParam("Quizgroup") UpdateQuizGroupRequest request);
+    ResponseEntity<ResultDto<Long>> updateQuizGroupV1(@RequestParam("Quizgroup") UpdateQuizGroupRequest request, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
 
-    ResponseEntity<ResultDto<Long>> removeQuizGroupV1(@PathVariable("quizgroupId") Long quizgroupId);
+    ResponseEntity<ResultDto<Long>> removeQuizGroupV1(@PathVariable("quizgroupId") Long quizgroupId, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
 }
--- src/main/java/Team9789/quizly_Spring/controller/api/quizgroup/QuizGroupApiControllerImpl.java
+++ src/main/java/Team9789/quizly_Spring/controller/api/quizgroup/QuizGroupApiControllerImpl.java
@@ -2,107 +2,93 @@ package Team9789.quizly_Spring.controller.api.quizgroup;
 
 import Team9789.quizly_Spring.dto.quiz.QuizGroupDto;
 import Team9789.quizly_Spring.dto.ResultDto;
-import Team9789.quizly_Spring.dto.user.UserDto;
 import Team9789.quizly_Spring.dto.request.quizgroup.CreateQuizGroupRequest;
 import Team9789.quizly_Spring.dto.request.quizgroup.UpdateQuizGroupRequest;
+import Team9789.quizly_Spring.jwt.JwtProvider;
 import Team9789.quizly_Spring.service.quizgroup.QuizGroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class QuizGroupApiControllerImpl implements QuizGroupApiController{
+public class QuizGroupApiControllerImpl implements QuizGroupApiController {
 
     private final QuizGroupService quizGroupService;
+    private final UserDetailsService userDetailsService;
+    private final JwtProvider jwtProvider;
 
-    /**
-     * 유저 이름으로 퀴즈 목록 조회
-     * 페이징 기능 추가되어 있음
-     */
     @GetMapping("/v1/users/{username}/quizgroups")
     public ResponseEntity<ResultDto<List<QuizGroupDto>>> findQuizGroupByUsernameV1(
             @PathVariable("username") String username,
-            @RequestParam("offset")  Integer offset,
+            @RequestParam("offset") Integer offset,
             @RequestParam("limit") Integer limit
     ) {
         List<QuizGroupDto> quizGroups = quizGroupService.getQuizGroupAllByUserName(username, offset, limit);
 
-        return new ResponseEntity<ResultDto<List<QuizGroupDto>>>(new ResultDto<>("유저 이름으로 퀴즈 목록 조회",quizGroups), HttpStatus.OK);
+        return new ResponseEntity<>(new ResultDto<>("유저 이름으로 퀴즈 목록 조회", quizGroups), HttpStatus.OK);
     }
 
-    /**
-     * 모든 퀴즈 목록 조회
-     * 페이지 기능 추가되어 있음
-     */
     @GetMapping("/v1/quizgroups")
     public ResponseEntity<ResultDto<List<QuizGroupDto>>> findQuizGroupAllV1(
-            @RequestParam("offset")  Integer offset,
+            @RequestParam("offset") Integer offset,
             @RequestParam("limit") Integer limit
     ) {
         List<QuizGroupDto> quizGroups = quizGroupService.getQuizGroupAll(offset, limit);
-        return new ResponseEntity<ResultDto<List<QuizGroupDto>>>(new ResultDto<>("모든 퀴즈 목록 조회",quizGroups), HttpStatus.OK);
+        return new ResponseEntity<>(new ResultDto<>("모든 퀴즈 목록 조회", quizGroups), HttpStatus.OK);
     }
 
-    /**
-     * 특정 퀴즈 그룹 조회
-     * 퀴즈 그룹 아이디를 이용해서 퀴즈 그룹 조회
-     */
     @GetMapping("/v1/quizgroups/{quizgroupId}")
     public ResponseEntity<ResultDto<QuizGroupDto>> findQuizGroupOneV1(@PathVariable("quizgroupId") Long quizgroupId) {
         QuizGroupDto quizGroupDto = quizGroupService.getQuizGroupOneById(quizgroupId);
-        return new ResponseEntity<ResultDto<QuizGroupDto>>(new ResultDto<>("특정 퀴즈 그룹 조회", quizGroupDto), HttpStatus.OK);
+        return new ResponseEntity<>(new ResultDto<>("특정 퀴즈 그룹 조회", quizGroupDto), HttpStatus.OK);
     }
 
-    /**
-     * 퀴즈 그룹 등록하기
-     * 등록된 퀴즈 그룹 ID 반환
-     */
     @PostMapping("/v1/quizgroups")
-    public ResponseEntity<ResultDto<Long>> addQuizGroupV1(@RequestBody CreateQuizGroupRequest request) {
-        // TODO: JWT에서 값을 꺼내오는 작업 해야 함
-        // 임시로 데이터 사용
-        UserDto userDto = createTemporaryUserEntity();
-        Long savedId = quizGroupService.saveQuizGroup(userDto ,request);
-        return new ResponseEntity<ResultDto<Long>>(new ResultDto<>("퀴즈 그룹 등록", savedId), HttpStatus.CREATED);
+    public ResponseEntity<ResultDto<Long>> addQuizGroupV1(
+            @RequestBody CreateQuizGroupRequest request,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
+    ) {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(new ResultDto<>("인증 실패", -1L), HttpStatus.UNAUTHORIZED);
+        }
+        String accessToken = authorizationHeader.substring(7);
+        int userId = Integer.parseInt(jwtProvider.getUserId(accessToken));
+
+        Long savedId = quizGroupService.saveQuizGroup(userId, request);
+        return new ResponseEntity<>(new ResultDto<>("퀴즈 그룹 등록", savedId), HttpStatus.CREATED);
     }
 
-    /**
-     * 퀴즈 그룹 수정하기
-     * 수정된 퀴즈 그룹 ID 반환
-     */
     @PutMapping("/v1/quizgroups/{quizgroupId}")
-    public ResponseEntity<ResultDto<Long>> updateQuizGroupV1(@RequestBody UpdateQuizGroupRequest request) {
-        // TODO: JWT에서 값을 꺼내오는 작업 해야 함
-        // 임시로 데이터 사용
-        UserDto userDto = createTemporaryUserEntity();
-        Long updatedId = quizGroupService.updateQuizGroup(userDto, request);
-        return new ResponseEntity<ResultDto<Long>>(new ResultDto<>("퀴즈 그룹 수정", updatedId), HttpStatus.OK);
+    public ResponseEntity<ResultDto<Long>> updateQuizGroupV1(@RequestBody UpdateQuizGroupRequest request,
+                                                             @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
+    ) {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(new ResultDto<>("인증 실패", -1L), HttpStatus.UNAUTHORIZED);
+        }
+        String accessToken = authorizationHeader.substring(7);
+        int userId = Integer.parseInt(jwtProvider.getUserId(accessToken));
+
+        Long updatedId = quizGroupService.updateQuizGroup(userId, request);
+        return new ResponseEntity<>(new ResultDto<>("퀴즈 그룹 수정", updatedId), HttpStatus.OK);
     }
 
-    /**
-     * 퀴즈 그룹 삭제하기
-     * 삭제된 퀴즈 그룹 ID 반환
-     */
     @DeleteMapping("/v1/quizgroups/{quizgroupId}")
-    public ResponseEntity<ResultDto<Long>> removeQuizGroupV1(@PathVariable("quizgroupId") Long quizgroupId) {
-        // TODO: JWT에서 값을 꺼내오는 작업 해야 함
-        // 임시로 데이터 사용
-        UserDto userDto = createTemporaryUserEntity();
+    public ResponseEntity<ResultDto<Long>> removeQuizGroupV1(@PathVariable("quizgroupId") Long quizgroupId,
+                                                             @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
+    ) {
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return new ResponseEntity<>(new ResultDto<>("인증 실패", -1L), HttpStatus.UNAUTHORIZED);
+        }
+        String accessToken = authorizationHeader.substring(7);
+        int userId = Integer.parseInt(jwtProvider.getUserId(accessToken));
 
-        quizGroupService.removeQuizGroup(userDto, quizgroupId);
-        return new ResponseEntity<ResultDto<Long>>(new ResultDto<>("퀴즈 그룹 삭제", 0L), HttpStatus.NO_CONTENT);
+        quizGroupService.removeQuizGroup(userId, quizgroupId);
+        return new ResponseEntity<>(new ResultDto<>("퀴즈 그룹 삭제", 0L), HttpStatus.NO_CONTENT);
     }
-
-    // 임시 유저 데이터 사용
-    private UserDto createTemporaryUserEntity() {
-        UserDto userDto = new UserDto();
-        userDto.setUserId(1);
-        userDto.setUsername("홍길동");
-        return userDto;
-    }
-
 }
