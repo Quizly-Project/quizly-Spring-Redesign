5853ad0e73af1c86f441f8dc5c6407988ec3d577
yoo20370 <yoo20370@gmail.com>
2025-08-01 12:09:30 +0900

    [add] Swagger를 이용해서 CreateQuizRequestDto, CreateStudentResultRequest 설명 추가

--- src/main/java/Team9789/quizly_Spring/dto/request/quizresult/CreateQuizResultRequest.java
+++ src/main/java/Team9789/quizly_Spring/dto/request/quizresult/CreateQuizResultRequest.java
@@ -1,14 +1,18 @@
 package Team9789.quizly_Spring.dto.request.quizresult;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Schema(name="CreateQuizResultRequest", description = "퀴즈 결과 정보를 전달하는 DTO 객체")
 public class CreateQuizResultRequest {
-
+    @Schema(name="quizGroupId", description = "진행한 퀴즈 그룹 식별자")
     private Long quizGroupId;
+    @Schema(name="roomCode", description = "진행한 퀴즈의 퀴즈룸 방코드")
     private String roomCode;
+    @Schema(name="studentResults", description = "학생들의 퀴즈 풀이 정보 목록")
     private List<CreateStudentResultRequest> studentResults = new ArrayList<>();
 }
--- src/main/java/Team9789/quizly_Spring/dto/request/quizresult/CreateStudentResultRequest.java
+++ src/main/java/Team9789/quizly_Spring/dto/request/quizresult/CreateStudentResultRequest.java
@@ -1,11 +1,17 @@
 package Team9789.quizly_Spring.dto.request.quizresult;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(name="CreateStudentResultRequest", description = "학생들의 퀴즈 풀이 정보가 담긴 DTO 객체")
 public class CreateStudentResultRequest {
+    @Schema(name="nickname", description = "퀴즈를 푼 학생 이름")
     private String nickname;
+    @Schema(name="selectOption", description = "학생이 선택한 답안 JSON 형식")
     private String selectOption;
+    @Schema(name="result", description = "학생의 각 문제에 대한 정답 여부 JSON 형식")
     private String result;
+    @Schema(name="totalScore", description = "퀴즈에서 획득한 점수")
     private Integer totalScore;
 }
