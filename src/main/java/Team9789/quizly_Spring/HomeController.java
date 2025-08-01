0b7f5b24f83ebf6a995cd58aac8753e7dc5d667d
yoo20370 <yoo20370@gmail.com>
2025-08-01 12:55:37 +0900

    [add] HomeController Swagger API 문서에서 숨기기

--- src/main/java/Team9789/quizly_Spring/HomeController.java
+++ src/main/java/Team9789/quizly_Spring/HomeController.java
@@ -1,8 +1,10 @@
 package Team9789.quizly_Spring;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Hidden
 public class HomeController {
 
     public String home(){
