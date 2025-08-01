ae4a30b2a47231197a2c12b86795fbc3a2bdc12f
yoo20370 <yoo20370@gmail.com>
2025-08-01 16:06:38 +0900

    [add] Swagger를 이용해서 QuizGroupApiController, QuizResultApiController의 API 메서드 설명 및 가능한 응답(200,403,404...), 파라미터 설명 등 추가

--- src/main/java/Team9789/quizly_Spring/controller/api/quizgroup/QuizGroupApiController.java
+++ src/main/java/Team9789/quizly_Spring/controller/api/quizgroup/QuizGroupApiController.java
@@ -4,6 +4,10 @@ import Team9789.quizly_Spring.dto.quiz.QuizGroupDto;
 import Team9789.quizly_Spring.dto.ResultDto;
 import Team9789.quizly_Spring.dto.request.quizgroup.CreateQuizGroupRequest;
 import Team9789.quizly_Spring.dto.request.quizgroup.UpdateQuizGroupRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,25 +16,65 @@ import javax.naming.AuthenticationException;
 import java.util.List;
 
 @RestController
+@Tag(name="QuizGroupApiController", description = "퀴즈 그룹 정보를 CRUD하는 API")
 public interface QuizGroupApiController {
 
+    @Operation(summary = "유저이름으로 퀴즈그룹 목록 조회하는 API", description = "특정 사용자의 ID를 이용해서 퀴즈 그룹 목록 조회, 페이징 기능 제공")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공적으로 퀴즈 그룹 목록 조회"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
     ResponseEntity<ResultDto<List<QuizGroupDto>>> findQuizGroupByUsernameV1(
             @PathVariable("username") String username,
             @RequestParam("offset") Integer offset,
             @RequestParam("limit") Integer limit
     );
 
+    @Operation(summary = "모든 퀴즈 그룹 목록을 조회하는 API", description = "모든 퀴즈 그룹 목록을 조회, 페이징 기능 제공")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공적으로 퀴즈 그룹 목록 조회"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
     @GetMapping("/api/v1/quizgroups")
     ResponseEntity<ResultDto<List<QuizGroupDto>>> findQuizGroupAllV1(
             @RequestParam("offset") Integer offset,
             @RequestParam("limit") Integer limit
     );
 
+    @Operation(summary = "퀴즈그룹 식별자를 이용해서 퀴즈그룹을 조회하는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공적으로 특정 퀴즈 그룹 조회"),
+            @ApiResponse(responseCode = "404", description = "해당 퀴즈 그룹을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
     ResponseEntity<ResultDto<QuizGroupDto>> findQuizGroupOneV1(@PathVariable("quizgroupId") Long quizgroupId);
 
+    @Operation(summary = "퀴즈 그룹 등록을 위한 API", description = "사용자가 작성한 퀴즈 그룹, 퀴즈들, 선택지들을 DB에 저장하는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "성공적으로 퀴즈 그룹을 등록했습니다."),
+            @ApiResponse(responseCode = "403", description = "AuthorizationHeader에 문제가 있습니다."),
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
     ResponseEntity<ResultDto<Long>> addQuizGroupV1(@RequestParam("Quizgroup") CreateQuizGroupRequest request, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) throws AuthenticationException;
 
+    @Operation(summary = "퀴즈 그룹 수정을 위한 API", description = "사용자가 수정한 퀴즈 그룹, 퀴즈들, 선택지들을 DB에 저장하는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공적으로 퀴즈 그룹을 수정했습니다."),
+            @ApiResponse(responseCode = "403", description = "AuthorizationHeader에 문제가 있습니다."),
+            @ApiResponse(responseCode = "403", description = "수정자와 리소스 작성자가 다릅니다."),
+            @ApiResponse(responseCode = "404", description = "해당 리소스를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
     ResponseEntity<ResultDto<Long>> updateQuizGroupV1(@RequestParam("Quizgroup") UpdateQuizGroupRequest request, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) throws AuthenticationException;
 
+    @Operation(summary = "퀴즈 그룹 삭제를 위한 API", description = "특정 퀴즈 그룹을 삭제하는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공적으로 퀴즈 그룹을 등록했습니다."),
+            @ApiResponse(responseCode = "403", description = "수정자와 리소스 작성자가 다릅니다."),
+            @ApiResponse(responseCode = "403", description = "AuthorizationHeader에 문제가 있습니다."),
+            @ApiResponse(responseCode = "404", description = "해당 리소스를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
     ResponseEntity<ResultDto<Long>> removeQuizGroupV1(@PathVariable("quizgroupId") Long quizgroupId, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) throws AuthenticationException;
 }
--- src/main/java/Team9789/quizly_Spring/controller/api/quizresult/QuizResultApiController.java
+++ src/main/java/Team9789/quizly_Spring/controller/api/quizresult/QuizResultApiController.java
@@ -3,6 +3,11 @@ package Team9789.quizly_Spring.controller.api.quizresult;
 import Team9789.quizly_Spring.dto.quiz.QuizResultDto;
 import Team9789.quizly_Spring.dto.ResultDto;
 import Team9789.quizly_Spring.dto.request.quizresult.CreateQuizResultRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.naming.AuthenticationException;
 
 @RestController
+@Tag(name="QuizResultApiController", description = "퀴즈 진행 결과를 저장하고 가져오는 API")
 public interface QuizResultApiController {
 
-    ResponseEntity<ResultDto<String>> saveQuizResultV1(@RequestParam("QuizResult")CreateQuizResultRequest request, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) throws AuthenticationException;
+    @Operation(summary = "퀴즈 진행 결과를 저장하는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "퀴즈 결과를 정상적으로 저장했습니다."),
+            @ApiResponse(responseCode = "404", description = "해당 리소스를 찾을 수 없습니다"),
+            @ApiResponse(responseCode = "403", description = "AuthorizationHeader에 문제가 있습니다."),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
+    ResponseEntity<ResultDto<String>> saveQuizResultV1(
+            @Parameter(description = "퀴즈가 끝난 뒤 퀴즈 결과가 담긴 DTO 객체", required = true)
+            @RequestParam("QuizResult") CreateQuizResultRequest request,
+            @Parameter(description = "HTTP Header에서 JWT 토큰을 꺼내기 위한 변수", required = false)
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) throws AuthenticationException;
 
-    ResponseEntity<ResultDto<QuizResultDto>> findQuizResultByRoomCodeV1(@PathVariable("roodCode") String roomCode);
+    @Operation(summary = "퀴즈 결과를 불러오는 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "퀴즈 결과를 정상적으로 가져왔습니다."),
+            @ApiResponse(responseCode = "404", description = "해당 리소스를 찾을 수 없습니다"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
+    ResponseEntity<ResultDto<QuizResultDto>> findQuizResultByRoomCodeV1(
+            @Parameter(description = "퀴즈방 번호", required = true, example = "324156")
+            @PathVariable("roodCode") String roomCode);
 }
