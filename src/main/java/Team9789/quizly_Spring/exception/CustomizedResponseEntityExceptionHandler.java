50db06e3704c7a63da00b2f8add962c3e9b2a6a3
yoo20370 <yoo20370@gmail.com>
2025-08-01 12:53:31 +0900

    [add] 회원가입시 이미 유저가 존재하는 예외, Validation 검증 예외, 나머지 예외 처리를 위한 공통 예외 핸들러 메서드 추가

--- src/main/java/Team9789/quizly_Spring/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/Team9789/quizly_Spring/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,10 @@
 package Team9789.quizly_Spring.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -24,5 +27,26 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public final ResponseEntity<Object> handleResourceOwnerMismatchException(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = ExceptionResponse.of(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+    }
+
+    // 409 응답
+    @ExceptionHandler(UserExistException.class)
+    public final ResponseEntity<Object> handleUserExistException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = ExceptionResponse.of(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
+    }
+
+    // 다른 예외 공통 처리
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = ExceptionResponse.of(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Validation 검증 예외 발생시
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ExceptionResponse exceptionResponse = ExceptionResponse.of(new Date(), ex.getMessage(), ex.getBindingResult().toString());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
