2a3aad6355adb0b81935244aafdc55ff97c20048
yoo20370 <yoo20370@gmail.com>
2025-08-01 12:23:23 +0900

    [add] ResultDto, JoinDto에 대하여 Swagger를 이용해 설명 추가

--- src/main/java/Team9789/quizly_Spring/dto/ResultDto.java
+++ src/main/java/Team9789/quizly_Spring/dto/ResultDto.java
@@ -1,13 +1,17 @@
 package Team9789.quizly_Spring.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 /**
  * JSON으로 여러 데이터를 전송하기 위해 사용하는 기 래퍼 클래스
  */
 @Getter
+@Schema(name="ResultDto", description = "사용자에게 동일한 응답을 제공하기 위한 DTO 객체")
 public class ResultDto<T> {
+    @Schema(name="message", description = "요청에 대한 서버에서 제공하는 간단한 message")
     private String message;
+    @Schema(name="data", description = "요청에 대한 응답 데이터")
     private T data;
 
 
--- src/main/java/Team9789/quizly_Spring/dto/user/JoinDTO.java
+++ src/main/java/Team9789/quizly_Spring/dto/user/JoinDTO.java
@@ -1,14 +1,19 @@
 package Team9789.quizly_Spring.dto.user;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Schema(name="JoinDto", description = "회원가입 할 때, 사용자로부터 정보를 수신하기 위한 RequestDto 객체")
 public class JoinDTO {
 
+    @Schema(name="username", description = "사용자 ID")
     private String username;
+    @Schema(name="password", description = "사용자 password")
     private String password;
+    @Schema(name="email", description = "사용자 Email")
     private String email;
 }
